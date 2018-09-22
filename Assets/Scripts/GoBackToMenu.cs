ac7585e8f66df5e84442ad9bbd4cd554fa7f42c3
Amanda Abedmamoore <abedm002@empress.csusm.edu>
2018-09-22 16:34:10 -0700

    First commit

---
+++ Assets/Scripts/GoBackToMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBackToMenu : MonoBehaviour {
+
+	public void MenuButton(string goBack){
+		SceneManager.LoadScene(goBack);
+	}
+
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public void PlayButton(string newGame){
+		SceneManager.LoadScene(newGame);
+	}
+
+	public void QuitButton(){
+		Application.Quit();
+	}
+}
